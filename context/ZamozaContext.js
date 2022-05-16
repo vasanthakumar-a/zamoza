8b62382c38fed45cab3c0d85d266ff503e9b46cb
vasanth117 <vasanthakumara117@gmail.com>
2022-05-16 23:02:16 +0530

    zamozaContext_updated

--- context/ZamozaContext.js
+++ context/ZamozaContext.js
@@ -1,5 +1,7 @@
 import { createContext, useState,useEffect } from "react"
 import { useMoralis, useMoralisQuery } from "react-moralis"
+import { zamozaAbi, zamozaCoinAddress } from '../lib/constants'
+import { ethers } from "ethers"
 
 export const ZamozaContext = createContext()
 
@@ -7,6 +9,12 @@ export const ZamozaProvider = ({children}) => {
     const [username,setUsername] = useState('')
     const [nickName,setNickname] = useState('')
     const [assets,setAssets] = useState([])
+    const [currentAccount,setCurrentAccount] = useState('')
+    const [tokenAmount,setTokenAmount] = useState('')
+    const [amountDue,setAmountDue] = useState('')
+    const [etherScanLink,setEtherScanLink] = useState('')
+    const [isLoading, setIsLoading] = useState(false)
+    const [balance, setBalance] = useState('')
 
     const {
         authenticate,
@@ -26,11 +34,14 @@ export const ZamozaProvider = ({children}) => {
     useEffect(()=>{
         ;(async()=>{
             if(isAuthenticated) {
+                await getBalance()
                 const currentUsername = await user?.get('nickname')
                 setUsername(currentUsername)
+                const account = await user?.get('ethAddress')
+                setCurrentAccount(account)
             }
         })()
-    },[isAuthenticated,user,username])
+    },[isAuthenticated,user,username,currentAccount])
 
     useEffect(() => {
         ;(async()=>{
@@ -54,6 +65,57 @@ export const ZamozaProvider = ({children}) => {
         }
     }
 
+    const getBalance = async () => {
+        try {
+            if (!isAuthenticated || !currentAccount) return
+            
+            const options = {
+                contractAddress: zamozaCoinAddress,
+                functionName: 'balanceOf',
+                abi: zamozaAbi,
+                params: {
+                    account: currentAccount
+                },
+            }
+
+            if(isWeb3Enabled) {
+                const response = await Moralis.executeFunction(options)
+                setBalance(response.toString())
+            }
+        } catch(error) {
+            console.log(error)
+        }
+    }
+
+    const buyTokens = async() => {
+        try {
+            if (!isAuthenticated) {
+                await authenticate()
+            }
+            const amount = ethers.BigNumber.from(tokenAmount)
+            const price = ethers.BigNumber.from('100000000000000')
+            const calcPrice = amount.mul(price)
+
+            const options = {
+                contractAddress: zamozaCoinAddress,
+                functionName: 'mint',
+                abi: zamozaAbi,
+                msgValue: calcPrice,
+                params: {
+                    amount,
+                }
+            }
+
+            const transaction = await Moralis.executeFunction(options)
+            const recipt = await transaction.wait(4)
+            setIsLoading(false)
+            console.log(recipt)
+            setEtherScanLink(`https://rinkeby.etherscan.io/tx/${receipt.transactionHash}`)
+        } catch(error) {
+            console.log(error)
+        }
+    }
+
     const getAssets = async() => {
         try{
             await enableWeb3
@@ -73,6 +135,17 @@ export const ZamozaProvider = ({children}) => {
             username,
             handleSetUsername,
             assets,
+            buyTokens,
+            getBalance,
+            balance,
+            setTokenAmount,
+            tokenAmount,
+            amountDue,
+            setAmountDue,
+            isLoading,
+            setIsLoading,
+            setEtherScanLink,
+            etherScanLink,
         }}
         >
             {children}
